690db68bdf8f79d278fd04a9b7d5c72315b7869a
BenPaulat <benjamin.d.paulat@gmail.com>
2021-10-03 18:04:42 -0500

    Changed Genre to Dropdown menu, completed function to build API request

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,17 +5,21 @@ let rapidApiKey = "5d49265088msh6b983bed1315af1p14dfcbjsnaf5a9469ed5a"
 let rapidApiHost = "streaming-availability.p.rapidapi.com"
 
 // on click function to build Movie Streaming API call
-$(document).ready(function(){
-    $('#searchBtn').on('click', function(){
-        var movieGenreEntry = $("#genre").val().trim();
-        var movieKeyWordEntry = $("#key-word").val().trim();
-        rapidApiRequest(movieKeyWordEntry, movieGenreEntry);
-    })
+$('#searchBtn').on('click', function(){
+    var movieGenreEntry = $("#genre").val();
+    var movieKeyWordEntry = $("#key-word").val().trim();
+    rapidApiRequest(movieKeyWordEntry, movieGenreEntry);
 })
+
 
 // call function for Movie Streaming API
 const rapidApiRequest = function(movieKeyWordEntry, movieGenreEntry) {
-    fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&language=en", {
+    let streamingApiUrl = 
+        "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&page=1&language=en"
+        +"&genre=" + movieGenreEntry
+        +"&keyword=" + movieKeyWordEntry
+
+    fetch(streamingApiUrl, {
 	    "method": "GET",
 	    "headers": {
 		    "x-rapidapi-host": rapidApiHost,
