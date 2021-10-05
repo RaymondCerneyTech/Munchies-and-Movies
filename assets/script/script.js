8bb4846c9c8f54cbb27038c5807dc04006e89e43
BenPaulat <benjamin.d.paulat@gmail.com>
2021-10-04 20:21:46 -0500

    added appending function

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,17 +7,17 @@ let rapidApiHost = "streaming-availability.p.rapidapi.com"
 // on click function to build Movie Streaming API call
 $('#searchBtn').on('click', function(){
     var movieGenreEntry = $("#genre").val();
-    // var movieServiceEntry =$("#service").val();
+    var movieServiceEntry =$("#service").val();
     var movieKeyWordEntry = $("#key-word").val().trim();
-    rapidApiRequest(movieKeyWordEntry, movieGenreEntry);
+    rapidApiRequest(movieKeyWordEntry,movieServiceEntry, movieGenreEntry);
 })
 
 
 // call function for Movie Streaming API
-const rapidApiRequest = function(movieKeyWordEntry, movieGenreEntry) {
+const rapidApiRequest = function(movieKeyWordEntry, movieServiceEntry, movieGenreEntry) {
     let streamingApiUrl = 
-        "https://streaming-availability.p.rapidapi.com/search/basic?country=us&type=movie&page=1&language=en&service=netflix"
-        // +"&service="+ movieServiceEntry
+        "https://streaming-availability.p.rapidapi.com/search/basic?country=us&type=movie&page=1&language=en"
+        +"&service="+ movieServiceEntry
         +"&genre=" + movieGenreEntry
         +"&keyword=" + movieKeyWordEntry
 
@@ -32,9 +32,7 @@ const rapidApiRequest = function(movieKeyWordEntry, movieGenreEntry) {
     // handle results of Movie API call
     .then(function(response){
         response.json().then(function(data){
-            // movieResults(data);
-            console.log(data);
-            console.log(data.results[0].posterURLs[0]);
+            movieResults(data);
         });
     })    
     .catch(err => {
@@ -49,6 +47,6 @@ const movieResults = function(data) {
         $("#movie-results").append($("<div id='movie-" + i + "' class='grid grid-rows mt-5 text-lg leading-8'></div"));
         $("#movie-" + i).append($("<h3 id='movie-name" + i + "'>" + data.results[i].title + "</h3>"));
         $("#movie-" + i).append($("<p>" + data.results[i].overview +"</p>"));
-        $("#movie-" + i).append($("img='" + data.results[i].posterURLs[0]))
+        $("#movie-" + i).append($("<img src='" + data.results[i].posterURLs[92] + "'/>"));
     }
 }
